01e8eab739bab0a05f086af5867fa24e95423949
Ryan Delaney <vitineth@gmail.com>
2018-11-03 12:19:32 +0000

    Adding suicide code
    After some crashes on the lab machine, I've added code to kill the program after 10 seconds of execution. This is only temporary until we can confirm that it is stable and works successfully.

--- src/stacs/hackthebubble/cookie/Launcher.java
+++ src/stacs/hackthebubble/cookie/Launcher.java
@@ -4,6 +4,16 @@ public class Launcher {
 
     public static void main(String[] args) {
         new GameWindow().start();
+
+        // TODO(Ryan <vitineth>): 11/3/18  Remove in prod
+        new Thread(() -> {
+            try {
+                Thread.sleep(10000);
+            } catch (InterruptedException e) {
+                e.printStackTrace();
+            }
+            System.exit(1);
+        }).start();
     }
 
 }
