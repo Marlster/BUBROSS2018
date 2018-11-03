aa36fe23540a774f92f8c32fb8d36f9f4a2450fe
Ryan Delaney <vitineth@gmail.com>
2018-11-03 13:04:17 +0000

    Adding living entities
    These are distinct in that they carry health as well as sprite information. There are some utility functions for healing and damaging implemented but this just the bare bones basic template for now.

---
+++ src/stacs/hackthebubble/cookie/entities/LivingEntity.java
@@ -0,0 +1,79 @@
+package stacs.hackthebubble.cookie.entities;
+
+import stacs.hackthebubble.cookie.graphics.sprites.Sprite;
+import stacs.hackthebubble.cookie.location.Coordinate;
+
+public class LivingEntity extends Entity {
+
+    /**
+     * The current health of the entity. It is upper bounded by {@link #maxHealth} but has no lower bound to allow for more complex death mechanics but death is declared
+     * at <= 0 unless overwritten
+     */
+    private int health;
+    /**
+     * The maximum possible health of the entity. -1 means no maximum
+     */
+    private int maxHealth;
+
+    public LivingEntity(Coordinate location, Sprite sprite, int health, int maxHealth) {
+        super(location, sprite);
+        this.health = health;
+        this.maxHealth = maxHealth;
+    }
+
+    public LivingEntity(Sprite sprite, int health, int maxHealth) {
+        super(sprite);
+        this.health = health;
+        this.maxHealth = maxHealth;
+    }
+
+    public LivingEntity(Coordinate location, Sprite sprite, boolean visible, int health, int maxHealth) {
+        super(location, sprite, visible);
+        this.health = health;
+        this.maxHealth = maxHealth;
+    }
+
+    public LivingEntity(Sprite sprite, boolean visible, int health, int maxHealth) {
+        super(sprite, visible);
+        this.health = health;
+        this.maxHealth = maxHealth;
+    }
+
+    /**
+     * Restores the health to maximum health. This will work even if {@link #maxHealth} is -1 so use with caution
+     */
+    public void heal() {
+        this.health = maxHealth;
+    }
+
+    /**
+     * Tried to heal the entity by the given amount of hit points. It it would exceed the maximum HP, it will just be set to the maximum (unless the maximum is -1).
+     *
+     * @param amount the amount of health to restore
+     */
+    public void heal(int amount) {
+        if (this.health + amount > this.maxHealth && this.maxHealth != -1)
+            this.health = this.maxHealth;
+        else
+            this.health += amount;
+    }
+
+    /**
+     * Damages the entity by the given amount of hit points
+     *
+     * @param amount the amount of hit points to remove
+     */
+    public void damage(int amount) {
+        this.health -= amount;
+    }
+
+    /**
+     * If this entity is dead (health is less than or equal to 0)
+     *
+     * @return if the entity is dead
+     */
+    public boolean isDead() {
+        return this.health <= 0;
+    }
+
+}
