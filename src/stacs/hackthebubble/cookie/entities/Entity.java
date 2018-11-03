a69772f45f8f8d6f6194e976eba6985a3744c1d9
Ryan Delaney <vitineth@gmail.com>
2018-11-03 12:51:27 +0000

    Creating entity
    This is the template for entities which are anything that should be drawn to the screen. These will eventually be implemented by the characters and pieces of the level.
    
    I've also added two utility functions to the screen which are for rendering sprites in a nicer fashion, hiding the Graphics2D implementation.

---
+++ src/stacs/hackthebubble/cookie/entities/Entity.java
@@ -0,0 +1,109 @@
+package stacs.hackthebubble.cookie.entities;
+
+import java.awt.Graphics2D;
+import stacs.hackthebubble.cookie.graphics.Screen;
+import stacs.hackthebubble.cookie.graphics.sprites.Sprite;
+import stacs.hackthebubble.cookie.location.Coordinate;
+
+public class Entity implements Renderable {
+
+    /**
+     * The location on the screen that this entity should be drawn at
+     */
+    private Coordinate location;
+    /**
+     * The sprite that should be rendered to represent this entity
+     */
+    private Sprite sprite;
+
+    /**
+     * If the entity should be displayed by the renderer
+     */
+    private boolean visible;
+    /**
+     * If the entity should be removed on the next update run by the screen
+     */
+    private boolean markedForDeletion;
+
+    /**
+     * Constructs a new entity that is not marked for deletion and is visible with the given position and sprite
+     *
+     * @param location the location of the sprite on the render screen (top left [0, 0])
+     * @param sprite   the sprite that represents this entity
+     */
+    public Entity(Coordinate location, Sprite sprite) {
+        this(location, sprite, true);
+    }
+
+    /**
+     * Constructs a new entity that is not marked for deletion, is visible and located at (0, 0) with the given sprite
+     *
+     * @param sprite the sprite that represents this entity
+     */
+    public Entity(Sprite sprite) {
+        this(new Coordinate(), sprite, true);
+    }
+
+    /**
+     * Constructs a new entity that is not marked from deletion and has the given sprite, location and visibility status
+     *
+     * @param location the location of the sprite on the render screen (top left [0, 0])
+     * @param sprite   the sprite that represents this entity
+     * @param visible  the visibility status of this entity
+     */
+    public Entity(Coordinate location, Sprite sprite, boolean visible) {
+        this.location = location;
+        this.sprite = sprite;
+        this.visible = visible;
+    }
+
+    /**
+     * Constructs a new entity that is not marked for deletion, is at (0, 0) and has the given sprite and visibility status
+     *
+     * @param sprite  the sprite that represents this entity
+     * @param visible the visibility status of this entity
+     */
+    public Entity(Sprite sprite, boolean visible) {
+        this(new Coordinate(), sprite, visible);
+    }
+
+    @Override
+    public boolean isRendered() {
+        return visible;
+    }
+
+    @Override
+    public void render(Graphics2D g, Screen screen) {
+        screen.renderSprite(sprite, location, g);
+    }
+
+    @Override
+    public void onEvent(String event, Object... data) {
+    }
+
+    public boolean isMarkedForDeletion() {
+        return markedForDeletion;
+    }
+
+    /**
+     * Sets {@link #markedForDeletion} to true which means it should be deleted from view on the next update run. To remove it from view instantly, first use {@link #hide()}
+     * then call this function for proper cleanup
+     */
+    public void delete() {
+        markedForDeletion = true;
+    }
+
+    /**
+     * Hides the sprite from view by toggling the visibility status so the screen should skip the entity
+     */
+    public void hide() {
+        visible = false;
+    }
+
+    /**
+     * Shows the sprite by setting the visibility status so that the screen should begin rendering it
+     */
+    public void show() {
+        visible = true;
+    }
+}
--- src/stacs/hackthebubble/cookie/graphics/Screen.java
+++ src/stacs/hackthebubble/cookie/graphics/Screen.java
@@ -1,9 +1,12 @@
 package stacs.hackthebubble.cookie.graphics;
 
 import java.awt.Color;
+import java.awt.Dimension;
 import java.awt.Graphics2D;
 import java.util.Random;
 import stacs.hackthebubble.cookie.events.EventEmitter;
+import stacs.hackthebubble.cookie.graphics.sprites.Sprite;
+import stacs.hackthebubble.cookie.location.Coordinate;
 
 public class Screen {
 
@@ -53,5 +56,30 @@ public class Screen {
      */
     public void update() {
 
+    }
+
+    // Utility functions
+
+    /**
+     * Renders the given sprite at the given location using the grahic manager provided
+     *
+     * @param sprite   the sprite which needs to be drawn
+     * @param position the position on the screen that this should be drawn at
+     * @param g        the graphics object the sprite should be drawn to
+     */
+    public void renderSprite(Sprite sprite, Coordinate position, Graphics2D g) {
+        g.drawImage(sprite.getImage(), position.getX(), position.getY(), null);
+    }
+
+    /**
+     * Renders the given sprite, at the location and size provided using the given graphics instance
+     *
+     * @param sprite   the sprite which needs to be drawn
+     * @param position the position on the screen that this should be drawn at
+     * @param size     the size at which the sprite should be drawn, this should be multiple if you want it to produce high quality results
+     * @param g        the graphics object the sprite should be drawn to
+     */
+    public void renderSprite(Sprite sprite, Coordinate position, Dimension size, Graphics2D g) {
+        g.drawImage(sprite.getImage(), position.getX(), position.getY(), size.width, size.height, null);
     }
 }
